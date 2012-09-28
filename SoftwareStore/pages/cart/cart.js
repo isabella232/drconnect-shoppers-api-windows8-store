2a7d87c1f31d2bd90df93b3b7c1209b2cd040a73
cpryce <cpryce@digitalriver.com>
2012-09-28 11:49:04 -0500

    Fixed error in cart.js
        - Cart page now renders in blend
        - re-worked css/html on cart page template
        - created separate css files for product and category templates

--- SoftwareStore/pages/cart/cart.js
+++ SoftwareStore/pages/cart/cart.js
@@ -1,58 +1,68 @@
 ﻿(function () {
     "use strict";
 
-    if (Windows.ApplicationModel.DesignMode) {
-        var cartitems = [
-            {
-                "relation": "http:\/\/developers.digitalriver.com\/v1\/shoppers\/LineItemsResource",
-                "uri": "https:\/\/api.digitalriver.com\/v1\/shoppers\/me\/carts\/active\/line-items\/12861215142",
-                "id": 12861215142,
-                "quantity": 2,
-                "product": {
-                    "relation": "http:\/\/developers.digitalriver.com\/v1\/shoppers\/ProductsResource",
-                    "uri": "https:\/\/api.digitalriver.com\/v1\/shoppers\/me\/products\/248217400",
-                    "id": 248217400,
-                    "displayName": "Photo Editor",
-                    "thumbnailImage": "http:\/\/drh1.img.digitalriver.com\/DRHM\/Storefront\/Company\/aqued\/images\/product\/thumbnail\/80x80photo.png"
+    var cartitems = [
+        {
+            "quantity": 2,
+            "product": {
+                "displayName": "Photo Editor",
+                "thumbnailImage": "http://drh1.img.digitalriver.com/DRHM/Storefront/Company/aqued/images/product/thumbnail/80x80photo.png"
+            },
+            "pricing": {
+                "listPrice": {
+                    "currency": "USD",
+                    "value": 3.00
                 },
-                "pricing": {
-                    "listPrice": {
-                        "currency": "USD",
-                        "value": 3.00
-                    },
-                    "listPriceWithQuantity": {
-                        "currency": "USD",
-                        "value": 6.00
-                    },
-                    "salePriceWithQuantity": {
-                        "currency": "USD",
-                        "value": 5.40
-                    },
-                    "formattedListPrice": "$3.00",
-                    "formattedListPriceWithQuantity": "$6.00",
-                    "formattedSalePriceWithQuantity": "$5.40"
-                }
+                "listPriceWithQuantity": {
+                    "currency": "USD",
+                    "value": 6.00
+                },
+                "salePriceWithQuantity": {
+                    "currency": "USD",
+                    "value": 5.40
+                },
+                "formattedListPrice": "$3.00",
+                "formattedListPriceWithQuantity": "$6.00",
+                "formattedSalePriceWithQuantity": "$5.40"
             }
-        ];
+        },
+        {
+            "quantity": 1,
+            "product": {
+                "displayName": "Photo Editor",
+                "thumbnailImage": "http://drh1.img.digitalriver.com/DRHM/Storefront/Company/aqued/images/product/thumbnail/80x80photo.png"
+            },
+            "pricing": {
+                "listPrice": {
+                    "currency": "USD",
+                    "value": 10.00
+                },
+                "listPriceWithQuantity": {
+                    "currency": "USD",
+                    "value": 10.00
+                },
+                "salePriceWithQuantity": {
+                    "currency": "USD",
+                    "value": 9.99
+                },
+                "formattedListPrice": "$10.00",
+                "formattedListPriceWithQuantity": "$10.00",
+                "formattedSalePriceWithQuantity": "$10.00"
+            }
+        }
+    ];
 
-    
-        var cartlist = new WinJS.BindingList(cartlist);
-        WinJS.Namespace.define("CartData", {
-            datasource: cartitems
-        });
-
-    }
-
-    document.addEventListener("DOMContentLoaded", function (element, option) {
-        WinJS.UI.processAll();
+    var cartlist = new WinJS.Binding.List(cartitems);
+    WinJS.Namespace.define("CartData", {
+        listData: cartlist
     });
-    
 
-    WinJS.UI.Pages.define("/pages/category/category.html", {
-        ready: function () {
-            this.cartlist = element.querySelector("#carlist").winControl;
-            this.cartlist.itemTemplate = element.querySelector(".cart-item-template");
-            this.cartlist.datasource = CartData.datasource;
+    WinJS.UI.Pages.define("/pages/cart/cart.html", {
+        ready: function (element, options) {
+            this.itemsList = element.querySelector('.cartlist');
+            this.itemsList.itemTemplate = element.querySelector('#cartTemplate');
+            this.itemsList.layout = new WinJS.UI.ListLayout();
         }
     });
+
 }());
