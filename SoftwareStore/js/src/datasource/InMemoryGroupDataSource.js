e1f3ee1db4265380594e018f49c90a6baf62fb58
Martin Moscovich <martin.moscovich@globallogic.com>
2012-10-03 19:00:24 -0300

    Clean up grouping list logic #2

---
+++ SoftwareStore/js/src/datasource/InMemoryGroupDataSource.js
@@ -0,0 +1,114 @@
+﻿(function () {
+    var InMemoryGroupDataAdapter = DR.Class.extend(
+        function (groupData) {
+            // Constructor
+            this._groupData = groupData;
+        },
+
+        // Data Adapter interface methods
+        // These define the contract between the virtualized datasource and the data adapter.
+        // These methods will be called by virtualized datasource to fetch items, count etc.
+        {
+            // This example only implements the itemsFromIndex, itemsFromKey and count methods
+
+            // Called to get a count of the items, this can be async so return a promise for the count
+            getCount: function () {
+                var that = this;
+                return WinJS.Promise.wrap(that._groupData.length);
+            },
+
+            // Called by the virtualized datasource to fetch a list of the groups based on group index
+            // It will request a specific group and hints for a number of groups either side of it
+            // The implementation should return the specific group, and can choose how many either side
+            // to also send back. It can be more or less than those requested.
+            //
+            // Must return back an object containing fields:
+            //   items: The array of groups of the form:
+            //      [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
+            //   offset: The offset into the array for the requested group
+            //   totalCount: (optional) an update of the count of items
+            itemsFromIndex: function (requestIndex, countBefore, countAfter) {
+                var that = this;
+
+                if (requestIndex >= that._groupData.length) {
+                    return Promise.wrapError(new WinJS.ErrorFromName(UI.FetchError.doesNotExist));
+                }
+
+                var lastFetchIndex = Math.min(requestIndex + countAfter, that._groupData.length - 1);
+                var fetchIndex = Math.max(requestIndex - countBefore, 0);
+                var results = [];
+
+                // form the array of groups
+                for (var i = fetchIndex; i <= lastFetchIndex; i++) {
+                    var group = that._groupData[i];
+                    results.push({
+                        key: group.key,
+                        firstItemIndexHint: group.firstItemIndex,
+                        data: group
+                    });
+                }
+                return WinJS.Promise.wrap({
+                    items: results, // The array of items
+                    offset: requestIndex - fetchIndex, // The offset into the array for the requested item
+                    totalCount: that._groupData.length // The total count
+                });
+            },
+
+            // Called by the virtualized datasource to fetch groups based on the group's key
+            // It will request a specific group and hints for a number of groups either side of it
+            // The implementation should return the specific group, and can choose how many either side
+            // to also send back. It can be more or less than those requested.
+            //
+            // Must return back an object containing fields:
+            //   [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
+            //   offset: The offset into the array for the requested group
+            //   absoluteIndex: the index into the list of groups of the requested group
+            //   totalCount: (optional) an update of the count of items
+            itemsFromKey: function (requestKey, countBefore, countAfter) {
+                var that = this;
+                var requestIndex = null;
+
+                // Find the group in the collection
+                for (var i = 0, len = that._groupData.length; i < len; i++) {
+                    if (that._groupData[i].key === requestKey) {
+                        requestIndex = i;
+                        break;
+                    }
+                }
+                if (requestIndex === null) {
+                    return WinJS.Promise.wrapError(new WinJS.ErrorFromName(WinJS.UI.FetchError.doesNotExist));
+                }
+
+                var lastFetchIndex = Math.min(requestIndex + countAfter, that._groupData.length - 1);
+                var fetchIndex = Math.max(requestIndex - countBefore, 0);
+                var results = [];
+
+                //iterate and form the collection of the results
+                for (var j = fetchIndex; j <= lastFetchIndex; j++) {
+                    var group = that._groupData[j];
+                    results.push({
+                        key: group.key, // The key for the group
+                        firstItemIndexHint: group.firstItemIndex, // The index into the items for the first item in the group
+                        data: group // The data for the specific group
+                    });
+                }
+
+                // Results can be async so the result is supplied as a promise
+                return WinJS.Promise.wrap({
+                    items: results, // The array of items
+                    offset: requestIndex - fetchIndex, // The offset into the array for the requested item
+                    absoluteIndex: requestIndex, // The index into the collection of the item referenced by key
+                    totalCount: that._groupData.length // The total length of the collection
+                });
+            },
+
+        });
+
+    // Create a DataSource by deriving and wrapping the data adapter with a VirtualizedDataSource
+    WinJS.Namespace.define("DR.Store.Core.DataSource", {
+        InMemoryGroupDataSource: WinJS.Class.derive(WinJS.UI.VirtualizedDataSource, function (data) {
+            this.groups = data;
+            this._baseDataSourceConstructor(new InMemoryGroupDataAdapter(data));
+        })
+    });
+})();
