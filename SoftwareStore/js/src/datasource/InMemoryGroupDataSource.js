29bb2d6140d4b998bdc7cc284ca2f53281befb31
fsellecchia <fernando.sellecchia@globallogic.com>
2012-10-04 11:01:13 -0300

    Pagination (WIP)

--- SoftwareStore/js/src/datasource/InMemoryGroupDataSource.js
+++ SoftwareStore/js/src/datasource/InMemoryGroupDataSource.js
@@ -1,32 +1,45 @@
 ﻿(function () {
+    /**
+     * DataAdapter that contains the items specified on groupData
+     * groupData should contain {key: The Key of the group (corresponds to the groupKey in the itemsList),
+     *                           label: Group label that will be used to show in the list on the page
+     *                           firstItemIndex: First item on the list where the group begins
+     *
+     */
     var InMemoryGroupDataAdapter = DR.Class.extend(
         function (groupData) {
             // Constructor
             this._groupData = groupData;
         },
 
-        // Data Adapter interface methods
-        // These define the contract between the virtualized datasource and the data adapter.
-        // These methods will be called by virtualized datasource to fetch items, count etc.
+        /**
+         * Data Adapter interface methods
+         * These define the contract between the virtualized datasource and the data adapter.
+         * These methods will be called by virtualized datasource to fetch items, count etc.
+         */
         {
-            // This example only implements the itemsFromIndex, itemsFromKey and count methods
 
-            // Called to get a count of the items, this can be async so return a promise for the count
+            /**
+             * Called to get a count of the items, this can be async so return a promise for the count
+             * The count is the length of groupData
+             */
             getCount: function () {
                 var that = this;
                 return WinJS.Promise.wrap(that._groupData.length);
             },
 
-            // Called by the virtualized datasource to fetch a list of the groups based on group index
-            // It will request a specific group and hints for a number of groups either side of it
-            // The implementation should return the specific group, and can choose how many either side
-            // to also send back. It can be more or less than those requested.
-            //
-            // Must return back an object containing fields:
-            //   items: The array of groups of the form:
-            //      [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
-            //   offset: The offset into the array for the requested group
-            //   totalCount: (optional) an update of the count of items
+            /**
+             * Called by the virtualized datasource to fetch a list of the groups based on group index
+             * It will request a specific group and hints for a number of groups either side of it
+             * The implementation should return the specific group, and can choose how many either side
+             * to also send back. It can be more or less than those requested.
+             *
+             * Must return back an object containing fields:
+             *   items: The array of groups of the form:
+             *      [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
+             *   offset: The offset into the array for the requested group
+             *   totalCount: (optional) an update of the count of items
+             */
             itemsFromIndex: function (requestIndex, countBefore, countAfter) {
                 var that = this;
 
@@ -54,16 +67,18 @@
                 });
             },
 
-            // Called by the virtualized datasource to fetch groups based on the group's key
-            // It will request a specific group and hints for a number of groups either side of it
-            // The implementation should return the specific group, and can choose how many either side
-            // to also send back. It can be more or less than those requested.
-            //
-            // Must return back an object containing fields:
-            //   [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
-            //   offset: The offset into the array for the requested group
-            //   absoluteIndex: the index into the list of groups of the requested group
-            //   totalCount: (optional) an update of the count of items
+            /**
+             *Called by the virtualized datasource to fetch groups based on the group's key
+             * It will request a specific group and hints for a number of groups either side of it
+             * The implementation should return the specific group, and can choose how many either side
+             * to also send back. It can be more or less than those requested.
+             *
+             * Must return back an object containing fields:
+             *   [{ key: groupkey1, firstItemIndexHint: 0, data : { field1: value, field2: value, ... }}, { key: groupkey2, firstItemIndexHint: 27, data : {...}}, ...
+             *   offset: The offset into the array for the requested group
+             *   absoluteIndex: the index into the list of groups of the requested group
+             *   totalCount: (optional) an update of the count of items
+             */
             itemsFromKey: function (requestKey, countBefore, countAfter) {
                 var that = this;
                 var requestIndex = null;
