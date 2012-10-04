f54eb22eb4ebbc4d3d425248f7ee8a6850482c4c
Martin Moscovich <martin.moscovich@globallogic.com>
2012-10-04 18:12:13 -0300

    Template widgets WIP

---
+++ SoftwareStore/widgets/Widgets.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+    function create() {
+        var Class = WinJS.UI.Pages.define("/widgets/productTemplate/productTemplate.html", {
+            // This function is called whenever a user navigates to this page. It
+            // populates the page elements with the app's data.
+            ready: function (element, options) {},
+
+            unload: function () {},
+
+            updateLayout: function (element, viewState, lastViewState) {}
+        });
+        return Class;
+    }
+
+    WinJS.Namespace.define("DR.Store.Widget", {
+        ProductTemplate: create("productTemplate"),
+        CategoryTemplate: create("categoryTemplate")
+    });
+})();
---
+++ SoftwareStore/widgets/productTemplate/productTemplate.js
@@ -0,0 +1,27 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    var Class = WinJS.UI.Pages.define("/widgets/productTemplate/productTemplate.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+
+    WinJS.Namespace.define("DR.Store.Widget", {
+        ProductTemplate: Class
+    });
+})();
